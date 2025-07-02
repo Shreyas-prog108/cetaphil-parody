c0532fc04b0d242b0c4e92432e2110061b35a100
Shreyas Pandey <shreyaspandey.ethical@gmail.com>
2025-07-03 03:52:05 +0530

    mbile toggle added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 
-export const Navbar=()=>(
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
     <nav className="navbar">
-    <div className='navbar-logo'>Cetaphil</div> 
-  <ul className="navbar-links">
-    <li><a href="#home">Home</a></li>
-    <li><a href="#products">Products</a></li>
-    <li><a href="#" className="buy-now">Buy Now</a></li>
-  </ul>
-</nav>
-
-);
+      <div className='navbar-logo'>Cetaphil</div>
+      <button className="navbar-toggle" onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle menu">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <ul className={`navbar-links${menuOpen ? ' open' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#" className="buy-now">Buy Now</a></li>
+      </ul>
+    </nav>
+  );
+}
